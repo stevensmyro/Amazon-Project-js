7878f7eecd943fcd814ac9beeca34ea496a24bdb
stevensmyro <stevensmyro@gmail.com>
2025-06-04 02:11:35 +0300

    Made the cart quantity interactive!

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -77,10 +77,16 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
       cart.push({
         productId : productId,
         quantity: 1
-    });
+      });
     }
 
-    
-    console.log(cart)
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+  
   });
 });
