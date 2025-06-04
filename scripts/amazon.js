f0a0f8766cf4327d8c1bf716cc9d3748da441fc1
stevensmyro <stevensmyro@gmail.com>
2025-06-04 14:33:13 +0300

    Made select amount element work

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -26,7 +26,7 @@ products.forEach((product) => {
       </div>
 
       <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
           <option selected value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -59,8 +59,10 @@ console.log(productsHTML)
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
+
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
+    let quantity = button.parentElement.querySelector(`.js-quantity-selector-${button.dataset.productId}`);
     const productId = button.dataset.productId;
 
     let matchingItem;
@@ -72,11 +74,11 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     });
 
     if (matchingItem) {
-      matchingItem.quantity += 1
+      matchingItem.quantity += Number(quantity.value)
     } else {
       cart.push({
         productId : productId,
-        quantity: 1
+        quantity: Number(quantity.value)
       });
     }
 
